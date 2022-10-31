d274d69f723e44323d95293506c509beb9b83e79
RashaAlharthi <russa.m99@gmail.com>
2022-10-31 22:52:12 +0300

    updating ..

--- ML_Deployment_Assignment_By_Rasha/app.py
+++ ML_Deployment_Assignment_By_Rasha/app.py
@@ -6,8 +6,9 @@ import pickle
 app = Flask(__name__)
 
 # Loading our model and encoder 
-encoder = pickle.load(open('encoder.pkl', 'rb'))
-model = pickle.load(open('model.pkl', 'rb'))
+encoder = pickle.load(open('notebook/encoder.pkl', 'rb'))
+scaler = pickle.load(open('notebook/scaler.pkl', 'rb'))
+model = pickle.load(open('notebook/model.pkl', 'rb'))
 
 # This will take you to the home HTML page --> in this example it is called home.html
 # Note: in general applications home pages are usually called --> index.html
@@ -36,13 +37,17 @@ def predict():
     df2 = pd.DataFrame(data=num_features)
 
     # Encode strings
-    enc_arr = encoder.transform(df1) 
+#    enc_arr = encoder.transform(df1)    
+    cols = df1.columns.tolist()
+    for column in cols:
+        df1[column] = encoder.fit_transform(df1[column])
     
     # Add encoded values to dataframe
-    df2[encoder.get_feature_names()] = enc_arr.toarray()
+#    df2[encoder.get_feature_names()] = enc_arr.toarray()
+    df1 = df1.join(df2)
     
     # Make predictions on dataframe
-    prediction = model.predict(df2)
+    prediction = model.predict(df1)
     # get the output
     output = prediction[0]
 
